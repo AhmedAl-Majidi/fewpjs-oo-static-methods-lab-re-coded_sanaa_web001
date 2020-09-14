e24efe2e17b753cd09e54eaccf0d5f217c38e75f
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-14 16:35:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,21 @@ class Formatter {
   static capitalize(name) {
     return name.charAt(0).toUpperCase() + name.slice(1);
   }
+
+  static sanitize (string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g,'')
+  }
+
+  function titleize (sentence) {
+  if(!sentence.split) return sentence;
+  let tWord = function(string) {
+          return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+      },
+      result = [];
+  sentence.split(" ").forEach(function(w) {
+      result.push(tWord(w));
+  });
+  return result.join(" ");
+}
+
 }
